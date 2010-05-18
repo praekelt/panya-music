edafb370e666ffe99a0cdf0d5cf68d0656593c44
qoda <jonathan@blu-marble.co.za>
2010-05-18 16:03:45 +0200

    added generic object pop-up view

--- music/views.py
+++ music/views.py
@@ -1,8 +1,17 @@
 from content.generic.views import GenericObjectDetail
-from music.models import AudioEmbed
+from music.models import AudioEmbed, Track
 
 class ListenLive(GenericObjectDetail):
     def get_queryset(self):
         return AudioEmbed.permitted.all()
     
 listen_live = ListenLive()
+
+class PopupObjectDetail(GenericObjectDetail):
+    def get_queryset(self):
+        return Track.permitted.all()
+    
+    def get_template_name(self):
+        return "music/popup/music_detail.html"
+        
+popup_object_detail = PopupObjectDetail()
