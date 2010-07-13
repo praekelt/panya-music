293ada924d4edfcbb82aa55f96157f3be7e6128b
shaunsephton <shaunsephton@gmail.com>
2010-07-13 11:49:35 +0200

    popup moved to panya base views

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,4 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'music.views',
     url(r'^listen-live/$', 'listen_live', {'object_id': getattr(settings, 'LISTEN_LIVE_AUDIO_EMBED_ID', '-1') }, name='music_listen_live'),
-    url(r'^popup/(?P<slug>[\w-]+)/$', 'popup_object_detail', name='music_popup_object_detail'),
 )
