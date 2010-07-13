e0ac29b7bd8d54bfe6dfd2162d0e03f4f3a79d0c
shaunsephton <shaunsephton@gmail.com>
2010-07-13 11:49:21 +0200

    popup moved to panya base views

--- music/views.py
+++ music/views.py
@@ -1,17 +1,8 @@
 from panya.generic.views import GenericObjectDetail
-from music.models import AudioEmbed, Track
+from music.models import AudioEmbed
 
 class ListenLive(GenericObjectDetail):
     def get_queryset(self):
         return AudioEmbed.permitted.all()
     
 listen_live = ListenLive()
-
-class PopupObjectDetail(GenericObjectDetail):
-    def get_queryset(self):
-        return Track.permitted.all()
-    
-    def get_template_name(self):
-        return "music/popup/music_detail.html"
-        
-popup_object_detail = PopupObjectDetail()
