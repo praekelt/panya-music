659007a66607506feb2c7dcd8f0c0c047ab093af
shaunsephton <shaunsephton@gmail.com>
2010-07-16 08:55:49 +0200

    refactor for new generic views

--- music/views.py
+++ music/views.py
@@ -2,7 +2,7 @@ from panya.generic.views import GenericObjectDetail
 from music.models import AudioEmbed
 
 class ListenLive(GenericObjectDetail):
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         return AudioEmbed.permitted.all()
     
 listen_live = ListenLive()
