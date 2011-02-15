5f91dda992403471a55f205f197032c2e1375683
shaunsephton <shaun@praekeltconsulting.com>
2011-02-15 13:42:58 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-music',
-    version='0.0.5',
+    version='0.0.6',
     description='Panya music app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
